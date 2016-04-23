6371653f779002c26d38586bd9dc955e1d35c938
Casey <kaze@berkeley.edu>
2016-04-23 00:39:30 -0700

    commit

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-    $(document).ready(function() {
-	    $(".welcome").hover(function() {
-	        $(this).css("color", "white");
-	        $(".rightside").css("color", "white");
-	        $(".vertical").css("color", "white");
-	        $("html").animate({
-	        backgroundColor: "#ffa64d",
-	        color: "#ffa64d"
-	      }, 1000);
-	        $("#next").css("pointer-events", "auto");
-	    });
-	});
+    $(".welcome").hover(function() {
+        $(this).css("color", "white");
+        $(".rightside").css("color", "white");
+        $(".vertical").css("color", "white");
+        $("#next").css("color", "white");
+        $("html").animate({
+        backgroundColor: "#ffa64d",
+        color: "#ffa64d"
+      }, 1000);
+        $("#next").css("pointer-events", "auto");
+    });
 });
