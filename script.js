627e8ac1cb72728765419fbf6c964ae785fc171d
Casey <kaze@berkeley.edu>
2016-04-23 13:39:39 -0700

    star

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     if (window.innerHeight <= 400) { 
-        window.location.href = "/about";
+        window.location.replace("/about");
     }
     if (window.innerWidth <= 900) {  
-        window.location.href = "/about";
+        window.location.replace("/about");
     }
     $(window).on('resize', function() {
       var win = $(this); //this = window
       if (win.height() <= 400) {  
-        window.location.href = "/about";
+        window.location.replace("/about");
       }
       if (win.width() <= 900) { 
-        window.location.href = "/about";
+        window.location.replace("/about");
       }
     });
     $(".welcome").hover(function() {
