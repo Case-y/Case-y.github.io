f4d4c2443a5b13c15b737a3ca150fd89a6e92579
Casey <kaze@berkeley.edu>
2016-04-23 00:00:25 -0700

    hi

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-    $(".welcome").hover(function() {
-        $(this).css("color", "white");
-        $(".rightside").css("color", "white");
-        $(".vertical").css("color", "white");
-        $("html").animate({
-        backgroundColor: "#1c1717",
-        color: "#1c1717"
-      }, 1000);
-        $("#next").css("pointer-events", "auto");
-
-    });
+    $(document).ready(function() {
+	    $(".welcome").hover(function() {
+	        $(this).css("color", "white");
+	        $(".rightside").css("color", "white");
+	        $(".vertical").css("color", "white");
+	        $("html").animate({
+	        backgroundColor: "#ffa64d",
+	        color: "#ffa64d"
+	      }, 1000);
+	        $("#next").css("pointer-events", "auto");
+	    });
+	});
 });
